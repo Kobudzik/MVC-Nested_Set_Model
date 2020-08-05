64a2b6b3b5c4976607ec22e1113349bc0b8229bb
Kobudzik <kobudzik98@gmail.com>
2020-08-05 18:59:35 +0200

    Updated database, commented SEED method, reverted db data to initial, added initial migraion

---
+++ MVC_Adjacency_list_model/Migrations/202008051657094_SeedRootCategory.cs
@@ -0,0 +1,22 @@
+namespace MVC_Adjacency_list_model.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SeedRootCategory : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"
+                SET IDENTITY_INSERT [dbo].[Categories] ON
+                INSERT INTO [dbo].[Categories] ([ID], [Name], [lft], [rgt]) VALUES (1, N'Root', 1, 2)
+                SET IDENTITY_INSERT [dbo].[Categories] OFF
+            ");
+        }
+        
+        public override void Down()
+        {
+            Sql("DELETE FROM Categories");
+        }
+    }
+}
--- MVC_Adjacency_list_model/Migrations/Configuration.cs
+++ MVC_Adjacency_list_model/Migrations/Configuration.cs
@@ -13,28 +13,28 @@ namespace MVC_Adjacency_list_model.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(MVC_Adjacency_list_model.Models.ApplicationDbContext context)
-        {
-            //  This method will be called after migrating to the latest version.
+        //    protected override void Seed(MVC_Adjacency_list_model.Models.ApplicationDbContext context)
+        //    {
+        //        //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data.
+        //        //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+        //        //  to avoid creating duplicate seed data.
 
 
-            //DUMMY DATA- manually creating Dummy categories and sub categories  
-            context.Categories.AddOrUpdate(c => c.Name,
-             new Category { Name = "ROOT", lft = 1, rgt = 20 },
-             new Category { Name = "TELEVISIONS", lft = 2, rgt = 9 },
-             new Category { Name = "TUBE", lft = 3, rgt = 4 },
-             new Category { Name = "LCD", lft = 5, rgt = 6 },
-             new Category { Name = "PLASMA", lft = 7, rgt = 8 },
-             new Category { Name = "PORTABLE ELECTRONICS", lft = 10, rgt = 19 },
-             new Category { Name = "MP3 PLAYERS", lft = 11, rgt = 14 },
-             new Category { Name = "FLASH", lft = 12, rgt = 13 },
-             new Category { Name = "CD PLAYERS", lft = 15, rgt = 16 },
-             new Category { Name = "2 WAY RADIOS", lft = 17, rgt = 18 }
-             );
+        //        //DUMMY DATA- manually creating Dummy categories and sub categories  
+        //        context.Categories.AddOrUpdate(c => c.Name,
+        //         new Category { Name = "ROOT", lft = 1, rgt = 20 },
+        //         new Category { Name = "TELEVISIONS", lft = 2, rgt = 9 },
+        //         new Category { Name = "TUBE", lft = 3, rgt = 4 },
+        //         new Category { Name = "LCD", lft = 5, rgt = 6 },
+        //         new Category { Name = "PLASMA", lft = 7, rgt = 8 },
+        //         new Category { Name = "PORTABLE ELECTRONICS", lft = 10, rgt = 19 },
+        //         new Category { Name = "MP3 PLAYERS", lft = 11, rgt = 14 },
+        //         new Category { Name = "FLASH", lft = 12, rgt = 13 },
+        //         new Category { Name = "CD PLAYERS", lft = 15, rgt = 16 },
+        //         new Category { Name = "2 WAY RADIOS", lft = 17, rgt = 18 }
+        //         );
 
-        }
+        //    }
     }
 }
